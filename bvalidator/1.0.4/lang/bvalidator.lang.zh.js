e87c43f4e01db51d0898e7a4a43eed6f0656c602
WangXiaoJin <381954728@qq.com>
2016-11-21 22:01:55 +0800

    upgrade bvalidator

---
+++ bvalidator/1.0.4/lang/bvalidator.lang.zh.js
@@ -0,0 +1,25 @@
+
+bValidator.defaultOptions.messages.zh = {
+
+	'default' : '请输入正确格式',
+    minlen    : '至少输入{0}个字符',
+    maxlen    : '最多输入{0}个字符',
+    rangelen  : '输入字符数只能在{0}和{1}之间',
+    min       : '请输入一个大于或等于{0}的数值',
+    max       : '请输入一个小于或等于{0}的数值',
+    between   : '请输入一个介于{0}和{1}之间的数值',
+    required  : '此字段必填',
+    alpha     : '只能输入字母',
+    alphanum  : '只能输入字母和数字',
+    digit     : '只能输入数字',
+    number    : '请输入有效的数值',
+    email     : '请输入有效的电子邮件地址',
+    url       : '请输入有效的网址',
+    ip4       : '请输入有效的IPv4地址',
+    ip6       : '请输入有效的IPv6地址',
+    date      : '请输入有效的日期(格式:{0})',
+    equal     : '请确认两次输入的内容相同',
+    differ    : '请输入不同的值'
+};
+
+bValidator.defaultOptions.lang = 'zh';
